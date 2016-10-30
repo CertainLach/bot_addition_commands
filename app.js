a7d85031976eecc5cb2ed79ecc465b31daf6fe3e
F6CF <Creeplays@users.noreply.github.com>
2016-10-30 21:47:12 +0500

    Fixes and improvements
    Also look at new api:
    https://bot-sdk-docs.herokuapp.com/

--- app.js
+++ app.js
@@ -1,100 +1,90 @@
-var express = require('express');
-var cheerio = require('cheerio');
-var request = require('request');
-var htmlToText = require('html-to-text');
-var iconv = require('iconv-lite');
-var sleep = require('sleep');
+import express from 'express';
+import cheerio from 'cheerio';
+import request from 'request';
+import htmlToText from 'html-to-text';
+import iconv from 'iconv-lite';
+import sleep from 'sleep';
 
-var app = express();
+const app = express();
 
 b = "tmp global variable for temp values";
 
-
-
 function include(arr, num) {
-  return (arr.length > num);
+  return arr.length > num;
 }
 
-app.get('/', function(req, res) { //get light files function
+app.get('/', (req, res) => {
+  //get light files function
   //command example: .get https://raw.githubusercontent.com/Creeplays/LK.BIN-Parser/master/Readme.md
 
   b = req.query.msg.slice(req.query.msg.search(/\s/));
-  new Promise(function(resolve) {
-    var a = request(b, function(error, response, body) {
+  new Promise(resolve => {
+    const a = request(b, (error, response, body) => {
       if (!error) {
         resolve(body);
-      }
-      else {
+      } else {
         console.log(error);
       }
     });
-  }).then(function(response) {
-    console.log(response)
-    res.send("answer:" + response);
-  })
+  }).then(response => {
+    console.log(response);
+    res.send(`answer:${ response }`);
+  });
 });
 
-
-
-
-app.get('/google', function(req, res) { // googling function
+app.get('/google', (req, res) => {
+  // googling function
   // command example: .google github
 
   b = req.query.msg.slice(req.query.msg.search(/\s/) + 1);
   totalResults = 0; //needs define
-  new Promise(function(resolve) {
-    url1 = "https://www.google.ru/search?q=" + encodeURIComponent(b);
+  new Promise(resolve => {
+    url1 = `https://www.google.ru/search?q=${ encodeURIComponent(b) }`;
     request({
       uri: url1,
       method: 'GET',
       encoding: 'binary'
-    }, function(error, response, body) {
+    }, (error, response, body) => {
       if (error) {
-        console.log("Не удалось получить страницу из за следующей ошибки: " + error);
+        console.log(`Не удалось получить страницу из за следующей ошибки: ${ error }`);
         return;
       }
       mylist = [];
 
-      var $ = cheerio.load(
-          iconv.encode(
-            iconv.decode(
-              new Buffer(body, 'binary'),
-              'win1251'),
-            'utf8')
-        ),
-        links = $(".r a");
+      const $ = cheerio.load(iconv.encode(iconv.decode(new Buffer(body, 'binary'), 'win1251'), 'utf8'));
 
-      links.each(function(i, link) {
-        var url = $(link).attr("href");
+      const links = $(".r a");
+
+      links.each((i, link) => {
+        let url = $(link).attr("href");
         url = url.replace("/url?q=", "").split("&")[0];
         //console.log(url);
-        if ((url.charAt(0) === "/") || (i = links.length)) {
-          resolve(mylist)
+        if (url.charAt(0) === "/" || (i = links.length)) {
+          resolve(mylist);
         }
-        mylist.push(url + " - " + htmlToText.fromString($(link).html()))
+        mylist.push(`${ url } - ${ htmlToText.fromString($(link).html()) }`);
         totalResults++;
       });
     });
-  }).then(function(response) {
+  }).then(response => {
 
-    while (("answer:" + response.join("\n") + "\n More: " + url1).length >= 1500) response.pop();
-    console.log(response.join("\n"))
-    console.log(("answer:" + response.join("\n") + "\n More: " + url1).length)
-    res.send("answer:" + response.join("\n") + "\n More: " + url1);
-  })
+    while (`answer:${ response.join("\n") }\n More: ${ url1 }`.length >= 1500) response.pop();
+    console.log(response.join("\n"));
+    console.log(`answer:${ response.join("\n") }\n More: ${ url1 }`.length);
+    res.send(`answer:${ response.join("\n") }\n More: ${ url1 }`);
+  });
 });
 
-
-app.get('/man', function(req, res) { // googling function
+app.get('/man', (req, res) => {
+  // googling function
   // command example: .memes блеать, become a men, bleat
 
 
   b = req.query.msg.slice(req.query.msg.search(/\s/) + 1);
 
-
   listb = b.split(', ');
-
-  var mass = new Array('блеать', 'подозрительно', 'повсюду', 'мтк', 'чтоесли', 'бухал', 'пиздабол', 'пиздюк', 'хуйхурма', 'заявление', 'какой');
+  
+  const mass = {};
 
   mass["блеать"] = "79265063";
   mass["какой"] = "79276954";
@@ -108,48 +98,35 @@ app.get('/man', function(req, res) { // googling function
   mass['бухал'] = "79273483";
   mass['заявление'] = "79276423";
 
+  console.log(`-->${ listb[0].trim() }<--`);
 
-  console.log("-->" + listb[0].trim() + "<--");
-
-  if ((listb[0].trim() == 'лист') || (listb[0].trim() == 'memes') || (listb[0].trim() == 'w')) {
-    res.send('answer:' + mass.join(" "))
-    return
-
+  if (listb[0].trim() == 'лист' || listb[0].trim() == 'memes' || listb[0].trim() == 'w') {
+    res.send(`answer:${ Object.keys(mass).join(" ") }`);
+    return;
   }
-
 
   temp = mass[listb[0].trim().replace(/\s/g, '')];
   if (!temp) {
-    url1 = "https://api.imgflip.com/caption_image?max_font_size=60&template_id=79265063&username=srt&password=kernels&text0=" + encodeURIComponent("нет такой команды") + "&text1=" + encodeURIComponent("блеать")
+    url1 = `https://api.imgflip.com/caption_image?max_font_size=60&template_id=79265063&username=srt&password=kernels&text0=${ encodeURIComponent("нет такой команды") }&text1=${ encodeURIComponent("блеать") }`;
+  } else {
 
-  }
-  else {
-
-    url1 = "https://api.imgflip.com/caption_image?max_font_size=60&template_id=" + temp + "&username=srt&password=kernels&text0=" +
-      (include(listb, 1) ? (encodeURIComponent(listb[1])) : " ") +
-      "&text1=" + (include(listb, 2) ? (encodeURIComponent(listb[2])) : " ")
-
+    url1 = `https://api.imgflip.com/caption_image?max_font_size=60&template_id=${ temp }&username=srt&password=kernels&text0=${ include(listb, 1) ? encodeURIComponent(listb[1]) : " " }&text1=${ include(listb, 2) ? encodeURIComponent(listb[2]) : " " }`;
   }
 
-  new Promise(function(resolve) {
+  new Promise(resolve => {
 
-    request(url1, function(error, response, body) {
+    request(url1, (error, response, body) => {
       if (!error) {
         resolve(body);
-      }
-      else {
+      } else {
         console.log(error);
       }
     });
-  }).then(function(response) {
+  }).then(response => {
 
-    console.log(response)
-    res.send("img: :" + JSON.parse(response).data.url.replace(/:/g, '|'));
-  })
+    console.log(response);
+    res.send(`img: :${ JSON.parse(response).data.url.replace(/:/g, '|') }`);
+  });
 });
 
-
-
-
-
-app.listen(8081, function() {});
+app.listen(8081, () => {});
